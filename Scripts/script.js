00a6dfca2392c82b42e6dafeb505c03306f23223
Skill99 <kevincorreia99@outlook.com>
2023-01-08 00:00:22 -0300

    week1

---
+++ Scripts/script.js
@@ -0,0 +1,9 @@
+let lastMod = new Date(document.lastModified);
+const year = lastMod.getFullYear();
+const month = lastMod.getMonth()+1;
+const day = lastMod.getDay()+1;
+const hours = lastMod.getHours();
+const minutes = lastMod.getMinutes();
+const seconds = lastMod.getSeconds();
+lastMod = ` Last Updated: ${month}/${day}/${year} ${hours}:${minutes}:${seconds}`
+date.innerHTML = lastMod;
