dfb3358c066e616f94e479c27efe52a66cd9628f
Skill99 <kevincorreia99@outlook.com>
2023-01-08 00:17:47 -0300

    Week1 bugfix

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,8 @@
 let lastMod = new Date(document.lastModified);
 const year = lastMod.getFullYear();
 const month = lastMod.getMonth()+1;
-const day = lastMod.getDay()+1;
+const day = lastMod.getDate();
+
 const hours = lastMod.getHours();
 const minutes = lastMod.getMinutes();
 const seconds = lastMod.getSeconds();
