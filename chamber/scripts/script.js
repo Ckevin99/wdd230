494f72b6cd31630cd1dbcda04459bdb8505a3e96
Skill99 <kevincorreia99@outlook.com>
2023-01-29 03:22:48 -0300

    Week 4 Close

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -13,14 +13,15 @@ document.querySelector("#lastmod").textContent = lastMod;
 //Display Current Time
 const time = new Date();
 const day = time.getDay();
-const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+const date = time.getDate();
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 let dayname = days[day]
 let month = time.getMonth()+1;
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 let monthname = months[month]
 const year = time.getFullYear();
-const fulltime = `${dayname}, ${day} ${monthname} ${year}`;
+const fulltime = `${dayname}, ${date} ${monthname} ${year}`;
 console.log(fulltime);
 document.querySelector("#currentdate").textContent =fulltime;
 
