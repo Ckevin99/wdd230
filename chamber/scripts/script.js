bca9c31d1893f4fbf0e81902b9ac2e1b38b4a142
Skill99 <kevincorreia99@outlook.com>
2023-01-28 22:20:48 -0300

    week 4 missing small

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,15 @@
+let lastMod = new Date(document.lastModified)
+const yearmod = lastMod.getFullYear();
+const monthmod = lastMod.getMonth()+1;
+const daymod = lastMod.getDate();
+
+const hoursmod = lastMod.getHours();
+const minutesmod = lastMod.getMinutes();
+const secondsmod = lastMod.getSeconds();
+lastMod = ` ${monthmod}/${daymod}/${yearmod} ${hoursmod}:${minutesmod}:${secondsmod}`;
+document.querySelector("#lastmod").textContent = lastMod;
+
+
 //Display Current Time
 const time = new Date();
 const day = time.getDay();
@@ -11,5 +23,21 @@ const year = time.getFullYear();
 const fulltime = `${dayname}, ${day} ${monthname} ${year}`;
 console.log(fulltime);
 document.querySelector("#currentdate").textContent =fulltime;
+
+//Change
+
+const mynave = document.querySelector('.navigation');
+const hamburg = document.querySelector('#btn');
+const letter = document.querySelector('#btn').textContent;
+
+hamburg.addEventListener('click', () =>{mynave.classList.toggle('navigationopen')}, false);
+
+
+function toggleMenu(){
+  console.log("xxx"); 
+}
+const btnham = document.getElementById('btn')
+btnham.onclick = toggleMenu;
+
 
 
